7fef0b9cf28691298f17d7ab7e7cfb68b71f1eaf
frullah <frullah12@gmail.com>
2019-06-11 04:24:20 +0700

    test: tag name

--- validator_test.go
+++ validator_test.go
@@ -3,37 +3,66 @@ package ginvalidator
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/go-playground/validator.v9"
 )
 
 type User struct {
-	Name string `binding:"required"`
+	Name string `json:"name" binding:"required"`
 }
 
 func TestEngine_engineEqualToValidator(t *testing.T) {
 	engine := Validator{}
-	assert.Equal(t, engine.Engine(), engine.validate)
+	require.Equal(t, engine.Engine(), engine.validate)
 }
 
 func TestValidateStruct_valid(t *testing.T) {
 	engine := Validator{}
 	data := User{Name: "fajar"}
-	assert.NoError(t, engine.ValidateStruct(&data))
+	require.NoError(t, engine.ValidateStruct(&data))
 }
 
 func TestValidateStruct_invalid(t *testing.T) {
 	engine := Validator{}
 	data := User{Name: ""}
-	assert.Error(t, engine.ValidateStruct(&data))
+	require.Error(t, engine.ValidateStruct(&data))
+}
+
+func TestValidateStruct_jsonTag(t *testing.T) {
+	engine := Validator{}
+	data := User{Name: ""}
+	err := engine.ValidateStruct(&data)
+	require.Error(t, err)
+	errors := err.(validator.ValidationErrors)
+	require.Equal(t, errors[0].Field(), "name")
+}
+func TestValidateStruct_emptyJsonTag(t *testing.T) {
+	engine := Validator{}
+	data := struct {
+		Name string `binding:"required"`
+	}{Name: ""}
+	err := engine.ValidateStruct(&data)
+	require.Error(t, err)
+	errors := err.(validator.ValidationErrors)
+	require.Equal(t, errors[0].Field(), "Name")
+}
+func TestValidateStruct_dashJsonName(t *testing.T) {
+	engine := Validator{}
+	data := struct {
+		Name string `json:"-" binding:"required"`
+	}{Name: ""}
+	err := engine.ValidateStruct(&data)
+	require.Error(t, err)
+	errors := err.(validator.ValidationErrors)
+	require.Equal(t, errors[0].Field(), "Name")
 }
 
 func Test_lazyInit(t *testing.T) {
 	counter := 0
 	engine := Validator{ConfigFn: func(v *validator.Validate) { counter++ }}
 	engine.lazyinit()
-	assert.True(t, engine.initialized)
+	require.True(t, engine.initialized)
 
 	engine.lazyinit()
-	assert.Equal(t, 1, counter)
+	require.Equal(t, 1, counter)
 }
